42099cf5a6578ad1c211975cfa32adeeda31b4ac
Waqas <waqas.ahmed@zonetech.com.pk>
2020-02-21 23:38:46 +0500

    http

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const swaggerUi=require("swagger-ui-express");
 const swaggerDocument = require('./swagger.json');
 dotenv.config();
 var app = express();
+const http = require("http").createServer(app);
 app.use('/assets', express.static(path.join(__dirname,'web/assets')))
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json())
@@ -18,8 +19,10 @@ app.get('/api-docs', swaggerUi.setup(swaggerDocument));
 app.use(controllerRoutes);
 app.get("**", (req, res) => { res.sendFile(path.join(__dirname + "/web/pages/error.html")) })
 try {
-    app.listen(process.env.PORT);
-    console.log(`running at localhost:${process.env.PORT}`)
+    http.listen(process.env.PORT, '0.0.0.0', () => {
+        console.log(`running at localhost:${process.env.PORT}`);
+    });
+    
 } catch (e) {
 
 }
